fd85510e024c10564bc07073db6eb90ea5f1cef8
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-26 21:55:39 +0530

    [SaiVenkat]-Json Inventory Management

---
+++ JsonInventory/Model2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonInventory
+{
+    internal class Model2
+    {
+        public List<Model> Rice { get; set; }
+        public List<Model> Pulses { get; set; }
+        public List<Model> Wheat { get; set; }
+    }
+}
